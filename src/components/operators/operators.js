b139d4ce14c589d554219106a419dca622a8bd7f
Jafar <jafkam89@gmail.com>
2020-07-20 15:02:16 +0100

    first commit

---
+++ src/components/operators/operators.js
@@ -0,0 +1,125 @@
+import React from "react";
+
+import "./operators.css";
+
+class Operators extends React.Component {
+  render() {
+    const {
+      onClick,
+      add,
+      subtract,
+      divide,
+      multiply,
+      clear,
+      decimal,
+      equals,
+    } = this.props;
+
+    return (
+      <div className="buttons">
+        <button
+          name="7"
+          className="number"
+          onClick={(e) => onClick(e.target.name)}
+        >
+          {" "}
+          7
+        </button>
+        <button
+          name="8"
+          className="number"
+          onClick={(e) => onClick(e.target.name)}
+        >
+          8
+        </button>
+        <button
+          name="9"
+          className="number"
+          onClick={(e) => onClick(e.target.name)}
+        >
+          9
+        </button>
+        <button name="/" className="function" onClick={divide}>
+          /
+        </button>
+
+        <button
+          name="4"
+          className="number"
+          onClick={(e) => onClick(e.target.name)}
+        >
+          4
+        </button>
+        <button
+          name="5"
+          className="number"
+          onClick={(e) => onClick(e.target.name)}
+        >
+          5
+        </button>
+        <button
+          name="6"
+          className="number"
+          onClick={(e) => onClick(e.target.name)}
+        >
+          6
+        </button>
+        <button name="*" className="function" onClick={multiply}>
+          x
+        </button>
+
+        <button
+          name="1"
+          className="number"
+          onClick={(e) => onClick(e.target.name)}
+        >
+          1
+        </button>
+        <button
+          name="2"
+          className="number"
+          onClick={(e) => onClick(e.target.name)}
+        >
+          2
+        </button>
+        <button
+          name="3"
+          className="number"
+          onClick={(e) => onClick(e.target.name)}
+        >
+          3
+        </button>
+        <button name="-" className="function" onClick={subtract}>
+          -
+        </button>
+
+        <button
+          name="0"
+          className="number"
+          onClick={(e) => onClick(e.target.name)}
+        >
+          0
+        </button>
+        <button
+          name="."
+          className="number"
+          onClick={(e) => decimal(e.target.name)}
+        >
+          .
+        </button>
+        <button name="=" className="function" onClick={equals}>
+          =
+        </button>
+        <button name="+" className="function" onClick={add}>
+          +
+        </button>
+
+        <button name="CLEAR" className="clear" onClick={clear}>
+          CLEAR
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Operators;
---
+++ src/contianers/App.js
@@ -0,0 +1,139 @@
+import React from "react";
+import Operators from "../components/operators/operators";
+import "./App.css";
+// import "../components/operators/operators.css";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      display: "",
+      leftSide: "",
+      rightSide: "",
+      operator: "",
+      topDisplay: "0",
+    };
+  }
+
+  display = (e) => {
+    this.setState({ display: this.state.display + e });
+  };
+
+  decimal = (e) => {
+    let display = this.state.display;
+    try {
+      if (!display.includes(".")) {
+        this.setState({ display: display + e });
+      }
+    } catch (err) {
+      this.setState({ display: "Error" });
+    }
+  };
+
+  add = () => {
+    const leftSide = this.state.display;
+    this.setState({ leftSide, display: "", operator: "+" });
+    this.topDisplay();
+  };
+
+  subtract = () => {
+    const leftSide = this.state.display;
+    this.setState({ leftSide, display: "", operator: "-" });
+    this.topDisplay();
+  };
+
+  multiply = () => {
+    const leftSide = this.state.display;
+    this.setState({ leftSide, display: "", operator: "*" });
+    this.topDisplay();
+  };
+
+  divide = () => {
+    const leftSide = this.state.display;
+    this.setState({ leftSide, display: "", operator: "/" });
+    this.topDisplay();
+  };
+
+  topDisplay = () => {
+    this.setState((state) => ({
+      topDisplay: state.leftSide + state.operator + state.rightSide,
+    }));
+  };
+
+  clear = () => {
+    this.setState({
+      display: "",
+      topDisplay: "0",
+      leftSide: "",
+      rightSide: "",
+      operator: "",
+    });
+  };
+
+  calculate = () => {
+    const operator = this.state.operator;
+    const leftSide = this.state.leftSide;
+    let rightSide = this.state.rightSide;
+    rightSide = this.state.display;
+
+    try {
+      if (leftSide !== "") {
+        if (operator === "+") {
+          let result = parseFloat(leftSide) + parseFloat(rightSide);
+          this.setState({
+            rightSide: rightSide,
+            display: result,
+          });
+        } else if (operator === "-") {
+          let result = parseFloat(leftSide) - parseFloat(rightSide);
+          this.setState({
+            rightSide: rightSide,
+            display: result,
+          });
+        } else if (operator === "*") {
+          let result = parseFloat(leftSide) * parseFloat(rightSide);
+          this.setState({
+            rightSide: rightSide,
+            display: result,
+          });
+        } else if (operator === "/") {
+          let result = parseFloat(leftSide) / parseFloat(rightSide);
+          this.setState({
+            rightSide: rightSide,
+            display: result,
+          });
+        }
+      }
+    } catch (err) {
+      this.setState({ display: "Error" });
+    }
+    this.topDisplay();
+  };
+
+  render() {
+    return (
+      <div className="mainContainer">
+        <div className="container">
+          <div className="title">Hi I'm a Calculator</div>
+          <div className="displayContainer">
+            <div className="topDisplay">{this.state.topDisplay}</div>
+            <div className="display">{this.state.display}</div>
+          </div>
+          <Operators
+            onClick={this.display}
+            clear={this.clear}
+            add={this.add}
+            subtract={this.subtract}
+            divide={this.divide}
+            multiply={this.multiply}
+            decimal={this.decimal}
+            equals={this.calculate}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
